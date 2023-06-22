c051f183348e661c313b8c7b5bf4fa0ed7c9bacd
BryanDietrichBernhardt <bryandbernhardt@gmail.com>
2023-06-21 22:12:40 -0300

    fix(Api/Product): ajustar validações

--- src/database/entites/product.entity.ts
+++ src/database/entites/product.entity.ts
@@ -1,4 +1,5 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 @Entity()
 export class Product {
@@ -6,14 +7,53 @@ export class Product {
   id!: number;
 
   @Column()
+  @IsNotEmpty({
+    message: 'O código do produto deve ser declarado!',
+  })
+  @IsNumber(
+    {
+      maxDecimalPlaces: 0,
+    },
+    {
+      message: 'O código não deve conter casas decimais e ser um número.',
+    },
+  )
   code: number;
 
   @Column()
+  @IsNotEmpty({
+    message: 'O nome do produto deve ser declarado!',
+  })
+  @IsString({
+    message: 'O nome do produto deve ser um texto!',
+  })
   name: string;
 
   @Column()
+  @IsNotEmpty({
+    message: 'A quantidade do produto deve ser declarado!',
+  })
+  @IsNumber(
+    {
+      maxDecimalPlaces: 0,
+    },
+    {
+      message: 'A quantidade não deve conter casas decimais e ser um número.',
+    },
+  )
   quantity: number;
 
   @Column('double')
+  @IsNotEmpty({
+    message: 'O valor do produto deve ser declarado!',
+  })
+  @IsNumber(
+    {
+      maxDecimalPlaces: 2,
+    },
+    {
+      message: 'O valor deve ser um número e conter até 2 casas decimais.',
+    },
+  )
   value: number;
 }
