91b61efe7e0adee595b80e74afa89b0082b76222
BryanDietrichBernhardt <bryandbernhardt@gmail.com>
2023-06-26 22:35:59 -0300

    fix(Api): adicionar CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
